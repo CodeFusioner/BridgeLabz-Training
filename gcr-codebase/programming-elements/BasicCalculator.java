f0088f73566103641234f3cdfae84957ecddc2c5
Kapil Gupta <kg666235@gmail.com>
2025-12-18 15:46:46 +0530

    [Kapil Gupta] Programming Questions

--- gcr-codebase/programming-elements/BasicCalculator.java
+++ gcr-codebase/programming-elements/BasicCalculator.java
@@ -10,7 +10,6 @@ public class BasicCalculator  {
         double subtraction = num1 - num2;
         double multiplication = num1 * num2;
         double division = num1 / num2;
-
         System.out.println("The addition, subtraction, multiplication and division value of 2 numbers "
                 + num1 + " and " + num2 + " is "
                 + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
--- gcr-codebase/programming-elements/FeeDiscount.java
+++ gcr-codebase/programming-elements/FeeDiscount.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class FeeDiscount {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double fee = input.nextDouble();
-        double discountPercent = input.nextDouble();
+        Scanner sc = new Scanner(System.in);
+        double fee = sc.nextDouble();
+        double discountPercent = sc.nextDouble();
         double discount = (fee * discountPercent) / 100;
         double finalFee = fee - discount;
         System.out.println("The discount amount is INR " + discount +
