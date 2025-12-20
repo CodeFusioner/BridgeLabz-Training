0a758517672d42f225891367af82f8d15b0db2f8
Kapil Gupta <kg666235@gmail.com>
2025-12-20 16:37:20 +0530

    [Kapil Gupta] Arrays Questions

--- core-java-practice/gcr-codebase/arrays/VoteAge.java
+++ core-java-practice/gcr-codebase/arrays/VoteAge.java
@@ -1,7 +1,29 @@
-
+import java.util.*;
 public class VoteAge {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        
-    }
+	  public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int[] ages = new int[10];
+	        
+	        // Taking user input for 10 students
+	        System.out.println("Enter the ages of 10 students:");
+	        for (int i = 0; i < ages.length; i++) {
+	            System.out.print("Student " + (i + 1) + " age: ");
+	            ages[i] = sc.nextInt();
+	        }
+	        
+	        // Checking voting eligibility
+	        System.out.println("\nVoting Eligibility Results:");
+	        for (int i = 0; i < ages.length; i++) {
+	            int age = ages[i];
+	            
+	            if (age < 0) {
+	                System.out.println("Invalid age for student " + (i + 1));
+	            } else if (age >= 18) {
+	                System.out.println("The student with the age " + age + " can vote.");
+	            } else {
+	                System.out.println("The student with the age " + age + " cannot vote.");
+	            }
+	        }
+	        
+	    }
 }
