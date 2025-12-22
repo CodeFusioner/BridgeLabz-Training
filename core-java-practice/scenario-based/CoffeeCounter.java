b6ae4a8ad295db4ad061e02488f0f0b407a808fb
Kapil Gupta <kg666235@gmail.com>
2025-12-22 20:19:08 +0530

    [Kapil Gupta] String Questions

--- core-java-practice/scenario-based/CoffeeCounter.java
+++ core-java-practice/scenario-based/CoffeeCounter.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
 
 public class CoffeeCounter {
-
     // function to get price based on coffee type
     public static double getCoffeePrice(String coffeeType) {
-
         switch (coffeeType.toLowerCase()) {
             case "espresso":
                 return 120.0;
@@ -16,7 +14,6 @@ public class CoffeeCounter {
                 return -1; // if the coffee entered is invalid
         }
     }
-
     // function to calculate total bill with GST
     public static double calculateTotal(double price, int quantity) {
         double bill = price * quantity;
@@ -30,20 +27,16 @@ public class CoffeeCounter {
 
 //        keep on taking input from user until the user inputs exit
         while (true) {
-            System.out.print("\nEnter coffee type (Espresso/Latte/Cappuccino) or 'exit': ");
             String coffeeType = sc.nextLine();
-
             if (coffeeType.equalsIgnoreCase("exit")) {
                 System.out.println("Thank you!");
                 break;
             }
-
             double price = getCoffeePrice(coffeeType);
             if (price == -1) {
                 System.out.println("Invalid coffee type.");
                 continue;
             }
-
 //            get input value for quantity
             System.out.print("Enter quantity: ");
             int quantity = sc.nextInt();
