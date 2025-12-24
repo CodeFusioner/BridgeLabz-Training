2846c0df7d8bda2ee09e2381dd7e1ef336d91d7d
Kapil Gupta <kg666235@gmail.com>
2025-12-24 10:53:30 +0530

    [Kapil Gupta] Scenario based

--- core-java-practice/gcr-codebase/string/TrimLeadingnTrailingSpaces.java
+++ core-java-practice/gcr-codebase/string/TrimLeadingnTrailingSpaces.java
@@ -1,4 +1,3 @@
-package com.string;
 import java.util.Scanner;
 
 public class TrimLeadingnTrailingSpaces {
--- core-java-practice/gcr-codebase/string/VotingEligibility.java
+++ core-java-practice/gcr-codebase/string/VotingEligibility.java
@@ -1,4 +1,3 @@
-package com.string;
 import java.util.Scanner;
 import java.util.Random;
 
