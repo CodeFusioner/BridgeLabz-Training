de5394517c4aa28c18a3531d53581563a8ed0019
Kapil Gupta <kg666235@gmail.com>
2026-01-03 11:23:12 +0530

    [Kapil Gupta] Add. Inheritance Questions

---
+++ oops-practice/gcr-codebase/inheritance/VehicleTest.java
@@ -0,0 +1,74 @@
+class Vehicles {
+    int maxSpeed;
+    String fuelType;
+
+    Vehicles(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed);
+        System.out.println("Fuel Type: " + fuelType);
+    }
+}
+
+class Car extends Vehicles {
+    int seatCapacity;
+
+    Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+class Truck extends Vehicles {
+    int loadCapacity;
+
+    Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+class Motorcycle extends Vehicles {
+    boolean hasGear;
+
+    Motorcycle(int maxSpeed, String fuelType, boolean hasGear) {
+        super(maxSpeed, fuelType);
+        this.hasGear = hasGear;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Gear: " + hasGear);
+    }
+}
+
+public class VehicleTest {
+    public static void main(String[] args) {
+        Vehicles[] vehicles = {
+                new Car(180, "Petrol", 5),
+                new Truck(120, "Diesel", 10),
+                new Motorcycle(150, "Petrol", true)
+        };
+
+        for (Vehicles v : vehicles) {
+            v.displayInfo();
+            System.out.println();
+        }
+    }
+}
