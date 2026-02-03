3f452932fc2c90c520f80d1f6cdf73fd9db89773
Kapil Gupta <kg666235@gmail.com>
2026-02-03 16:03:22 +0530

    [Kapil Gupta] Add. Added streamApi Questions

---
+++ java8-programming-practice/streamApi/EmailNotification.java
@@ -0,0 +1,17 @@
+package streamApi;
+
+import java.util.List;
+
+public class EmailNotification {
+    public static void main(String[] args) {
+        List<String> emails = List.of(
+                "a@example.com",
+                "b@example.com",
+                "c@example.com"
+        );
+        emails.forEach(email -> sendEmailNotification(email));
+    }
+    static void sendEmailNotification(String email) {
+        System.out.println("Sending email to: " + email);
+    }
+}
